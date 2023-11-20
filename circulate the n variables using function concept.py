0d9cbddea6d3983751ab9e8834aeb55eda0b89c4
23011258 <yenchanindian63@gmail.com>
2023-11-20 09:53:31 +0530

    updated

---
+++ circulate the n variables using function concept.py
@@ -0,0 +1,8 @@
+Program to circulate N values.
+#Developed by: 
+#RegisterNumber:
+def circulate():
+    list1=eval(input())
+    n=int(input())
+    result=list1[n:]+list1[:n]
+    print("After circulating the values are:",result)
